8c6e08ab0f87eabb317bade12b4d8846b8e283c7
Igor Agapov <aio350@yahoo.com>
2024-08-19 13:17:07 +0500

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Box, createTheme, ThemeProvider } from '@mui/material'
 import CssBaseline from '@mui/material/CssBaseline'
-import { useEffect } from 'react'
+// import { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import { useLocalStorage } from 'react-use'
 import Nav from './components/Nav'
-import clear from './lib/clear'
-import seed from './lib/seed'
+// import clear from './lib/clear'
+// import seed from './lib/seed'
 
 export default function App() {
   const [mode, setMode] = useLocalStorage<'light' | 'dark'>(
@@ -14,25 +14,25 @@ export default function App() {
     'light',
   )
 
-  useEffect(() => {
-    const onKeydown = (e: KeyboardEvent) => {
-      if (e.key === 's' && (e.ctrlKey || e.metaKey)) {
-        e.preventDefault()
-        seed()
-      }
+  // useEffect(() => {
+  //   const onKeydown = (e: KeyboardEvent) => {
+  //     if (e.key === 's' && (e.ctrlKey || e.metaKey)) {
+  //       e.preventDefault()
+  //       seed()
+  //     }
 
-      if (e.key === 'c' && (e.ctrlKey || e.metaKey)) {
-        e.preventDefault()
-        clear()
-      }
-    }
+  //     if (e.key === 'c' && (e.ctrlKey || e.metaKey)) {
+  //       e.preventDefault()
+  //       clear()
+  //     }
+  //   }
 
-    window.addEventListener('keydown', onKeydown)
+  //   window.addEventListener('keydown', onKeydown)
 
-    return () => {
-      window.removeEventListener('keydown', onKeydown)
-    }
-  }, [])
+  //   return () => {
+  //     window.removeEventListener('keydown', onKeydown)
+  //   }
+  // }, [])
 
   const theme = createTheme({
     palette: {
